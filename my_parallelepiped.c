098e4d33a0dc7ec08cb3567ca4ec1d7c83d9482e
dogota_n <dogota_n@etna-alternance.net>
2019-11-04 14:17:21 +0100

    adding the first working version

--- my_parallelepiped.c
+++ my_parallelepiped.c
@@ -11,71 +11,71 @@ void make_spaces(int i);
 
 int my_parallelepiped(int x, int y, int z)
 {
-  if (z > 1) {
-    make_spaces(z - 1);
-    horizontal_line(x, y, 1);
-    my_putchar('\n');
-    if (z > 2) {
-      for (int i = z; i > 2; i--) {
-        make_spaces(i - 2);
-        depth_line(x, y, z, i);
-        my_putchar('\n');
+  if (x > 0 && y > 0 && z > 0) {
+    if (z > 1) {
+      make_spaces(z - 1);
+      horizontal_line(x, y, 1);
+      my_putchar('\n');
+      if (z > 2) {
+        for (int i = z; i > 2; i--) {
+          make_spaces(i - 2);
+          depth_line(x, y, z, i);
+          my_putchar('\n');
+        }
       }
     }
-  }
-  if (x > 0) {
-    horizontal_line(x, y, 2);
-    if (y == z && x > 1) {
-      make_spaces(z - 2);
-      my_putchar('a');
+    if (x > 0) {
+      horizontal_line(x, y, 2);
+      if (y == z && x > 1 && z > 1) {
+        make_spaces(z - 2);
+        my_putchar('*');
+      }
+      else if (z > y && y > 1) {
+        make_spaces(y - 2);
+        my_putchar('/');
+      }
+      else if (z < y && z > 1) {
+        make_spaces(z - 2);
+        my_putchar('|');
+      }
     }
-    else if (z > y && y > 1) {
-      make_spaces(y - 2);
-      my_putchar('/');
-    }
-    else if (z < y && z > 1) {
-      make_spaces(z - 2);
-      my_putchar('|');
+    if (y > 1 && x > 0) {
+      my_putchar('\n');
+      vertical_lines(x, y, z);
+      horizontal_line(x, y, 3);
     }
   }
-  if (y > 1 && x > 1) {
-    my_putchar('\n');
-    vertical_lines(x, y, z);
-  }
+  else
+    return (-1);
 }
 
 void horizontal_line(int x, int y, int number)
 {
-  if (x != 0) {
-    my_putchar('b');
-  }
+  if (x != 0)
+    my_putchar('*');
   for (int i = 0; i < (x - 2); i++) {
-    if (x > 2) {
+    if (x > 2)
       my_putchar('-');
-    }
-    else if (x > 0 && x <= 2) {
-      my_putchar('c');
-    }
+    else if (x > 0 && x <= 2)
+      my_putchar('*');
   }
-  if (x > 1) {
-    my_putchar('d');
-  }
+  if (x > 1)
+    my_putchar('*');
 }
 
 void depth_line(int x, int y, int z, int i)
 {
   my_putchar('/');
   make_spaces(x - 2);
-  if (x > 1) {
+  if (x > 1)
     my_putchar('/');
-  }
   if (i + y - 2 > z) {
     make_spaces(z - i);
     my_putchar('|');
   }
   else if (i + y - 2 == z) {
     make_spaces(z - i);
-    my_putchar('e');
+    my_putchar('*');
   }
   else if (i + y - 2 < z && y > 1) {
     make_spaces(y - 2);
@@ -88,20 +88,21 @@ void vertical_lines(int x, int y, int z)
   for (int i = 0, j = z - 3; i < y - 2; i++) {
     my_putchar('|');
     make_spaces(x - 2);
-    if (x > 1) {
-      my_putchar('|');
+    if (x > 0) {
+      if (x > 1)
+        my_putchar('|');
       if (y < z) {
-        make_spaces(j--);
+        make_spaces((j--) - (z - y));
         my_putchar('/');
       }
     }
-    if (y > z && i < y - z - 1) {
+    if (y > z && i < y - z - 1 && x > 1 && z > 1) {
       make_spaces(z - 2);
       my_putchar('|');
     }
     else if (i == y - z - 1) {
       make_spaces(z - 2);
-      my_putchar('z');
+      my_putchar('*');
     }
     else if (y > z && i > y - z - 1) {
       make_spaces(j--);
@@ -124,11 +125,7 @@ void make_spaces(int i)
 
 int main(int argc, char **argv)
 {
-  int x_input;
-  int y_input;
-  int z_input;
-
-  for (int i = 0, x_input = 0, y_input = 0, z_input = 0; i != 1;) {
+  for (int i = 0, x_input = 0, y_input = 0, z_input = 0; i != 1; z_input++) {
     printf("\ninsert the x_input :\n");
     scanf("%d", &x_input);
     printf("insert the y_input :\n");
